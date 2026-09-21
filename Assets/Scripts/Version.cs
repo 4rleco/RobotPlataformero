08a36b7b80700daea1bb7f904cfd306a2c7334fc
4rleco <117652001+4rleco@users.noreply.github.com>
2026-09-21 12:42:01 -0300

    Publish build

--- Assets/Scripts/Version.cs
+++ Assets/Scripts/Version.cs
@@ -1,7 +1,7 @@
 using System; 
 public static class Version 
 { 
-    private const String versionNumber = "v0.0.2"; 
+    private const String versionNumber = "v0.1.0"; 
  
     public static String GetVersionNumber() 
     { 
