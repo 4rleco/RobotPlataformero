7d6ce8e6f318a73d8f7d597f635bbcad483f14b2
LeonardoBrizuela99 <113455646+LeonardoBrizuela99@users.noreply.github.com>
2026-09-24 12:36:15 -0300

    Add change color random keyboard

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections; 
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,12 +13,36 @@ public class GameUI : MonoBehaviour
 
     [SerializeField] private PlayerActions player;
 
+    
+    private KeyCode lastJump;
+    private KeyCode lastDash;
+    private KeyCode lastCrouch;
+
     private void Update()
     {
+        
         jumpKey.text = "Jump Key: " + player.GetJumpKey();
         dashKey.text = "Dash Key: " + player.GetDashKey();
         crouchKey.text = "Crouch Key: " + player.GetCrouchKey();
         versionNumber.text = Version.GetVersionNumber();
+
+       
+        if (lastJump != KeyCode.None && player.GetJumpKey() != lastJump) StartCoroutine(FlashRed(jumpKey));
+        if (lastDash != KeyCode.None && player.GetDashKey() != lastDash) StartCoroutine(FlashRed(dashKey));
+        if (lastCrouch != KeyCode.None && player.GetCrouchKey() != lastCrouch) StartCoroutine(FlashRed(crouchKey));
+
+       
+        lastJump = player.GetJumpKey();
+        lastDash = player.GetDashKey();
+        lastCrouch = player.GetCrouchKey();
+    }
+
+    
+    private IEnumerator FlashRed(TextMeshProUGUI textMesh)
+    {
+        textMesh.color = Color.red;
+        yield return new WaitForSeconds(1.0f);
+        textMesh.color = Color.white;
     }
 
     public void Reset()
